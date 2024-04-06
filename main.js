8f2c4ccedfe5d32c9def4a0818fe76721470bb44
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-04-06 01:03:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const languageRegex = require(`./lib/wordlist.js`);
-module.exports = function check(word) {
+const badWordsList = require('./lib/badWordsList.json');
+function check(word) {
     let regex = new RegExp('\n' + word +'\n');
             if (languageRegex.match(regex)) {
                 return true;
             }
     return false;
 };
+function checkBad(text) {
+    const words = text.split(/\s+/);
+    for (const word of words) {
+        if (badWordsList.includes(word.toLowerCase())) {
+            return true;
+        }
+    }
+    return false; 
+}
+
+module.exports = { checkBad, check };
